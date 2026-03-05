f03b21e2c3a79100153fcd3a1caa132cca852102
Nikita Rybakov <rubakov.n92@gmail.com>
2026-03-05 08:28:53 +0200

    Did demo of search in messages window. Messages in progress

---
+++ src/main/java/org/controllers/ChatUserSearchResultController.java
@@ -0,0 +1,37 @@
+package org.controllers;
+
+import dto.ChatPreview;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import org.entities.User;
+
+public class ChatUserSearchResultController {
+
+    private User user;
+    private ChatController chatController;
+    private ChatSearchingContainerController cSCController;
+
+    @FXML
+    private Label teacherName;
+
+    public void setUser(User user){
+        this.user=user;
+        teacherName.setText(user.getFirstName() + " " + user.getSureName());
+    }
+
+    public void setChatController(ChatController chatController){
+        this.chatController = chatController;
+    }
+
+    public void setChatSearchingContainerController(ChatSearchingContainerController cSCController){
+        this.cSCController = cSCController;
+    }
+
+    @FXML
+    public void chooseTeacher(){
+        cSCController.closeModal();
+        ChatPreview chatPreview = new ChatPreview(user.getId(), user.getFirstName(), user.getSureName(), true);
+
+        chatController.addChatPreview(chatPreview);
+    }
+}
